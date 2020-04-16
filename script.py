d7b1778d792158d88c6d366bf7a6e05da9bb55f8
thomaspeter07 <thomaspeterhere@gmail.com>
2020-04-17 00:09:30 +0530

    1st python program

---
+++ script.py
@@ -0,0 +1,14 @@
+# assuming the age value as variable "character_age".
+# assuming the name value as variable "character_name".
+# assuming that because if anyone need to change the name and age of the person in the entire story.
+# for easy work.
+#john is the name that we wish to change to;
+#35 is the age that we wish to change to;
+# "john" is an example of string.
+character_name = "john"
+character_age = "55"
+is_male = True
+print("there was a man named " + character_name + ",")
+print("he was " + character_age + " years old.")
+print("he really liked the name " + character_name + ",")
+print("but he didn't like being " + character_age + ".")
