4fc17de7e82cc906ee8626ec3b85d8b3382773ed
thomaspeter07 <thomaspeterhere@gmail.com>
2020-04-17 14:54:39 +0530

    a small calculator

---
+++ calculator.py
@@ -0,0 +1,11 @@
+#created a variable called num1.and asked to enter up a value.
+num1 = input("enter a number: ")
+# created a variable called called num2.and asked to enter up the next value.
+num2 = input("enter another number: ")
+"""we created a variable "result" to print the answer.
+   we add num1 and num2.
+   then we use the float function to add the numbers even if we give decimal points.
+   but if we use int function only integers will be applicable for adding."""
+result = float(num1) + float(num2)
+print(result)
+
